6af864fc5feb28c2e91ee7ea476355c03584017b
Ryan Luu <ryan.luu@ryanluu.dev>
2025-06-20 21:38:55 -0400

    Fix GPS logic

--- RecorderAmbient/RecorderAmbient.ino
+++ RecorderAmbient/RecorderAmbient.ino
@@ -69,86 +69,83 @@ void setup() {
 }
 
 void loop() {
-  setLED(LOW, HIGH, LOW);  // GREEN
+  if (GPS.available()) {
+    setLED(LOW, HIGH, LOW);  // GREEN
 
-  if (!GPS.available()) {
-    Serial.println("GPS data not available!");
-    setLED(HIGH, LOW, LOW);  // RED
+    float temperature = ENV.readTemperature();  // MKR ENV Shield
+    float humidity = ENV.readHumidity();
+    float pressure = ENV.readPressure();
+    float illuminance = ENV.readIlluminance();
+    float latitude = GPS.latitude();  // MKR GPS Shield
+    float longitude = GPS.longitude();
+    float altitude = GPS.altitude();
+    float speed = GPS.speed();
+    int satellites = GPS.satellites();
+    unsigned long epochTime = GPS.getTime();
+
+    // Print sensor data to serial
+    Serial.print("Epoch time: ");
+    Serial.println(epochTime);
+
+    Serial.print("Temperature = ");  // MKR ENV Shield
+    Serial.print(temperature);
+    Serial.println(" °C");
+
+    Serial.print("Humidity = ");
+    Serial.print(humidity);
+    Serial.println(" %");
+
+    Serial.print("Pressure = ");
+    Serial.print(pressure);
+    Serial.println(" kPa");
+
+    Serial.print("Illuminance = ");
+    Serial.print(illuminance);
+    Serial.println(" lx");
+
+    Serial.print("Location = ");  // MKR GPS Shield
+    Serial.print(latitude, 7);
+    Serial.print(", ");
+    Serial.println(longitude, 7);
+
+    Serial.print("Altitude = ");
+    Serial.print(altitude);
+    Serial.println("m");
+
+    Serial.print("Ground speed = ");
+    Serial.print(speed);
+    Serial.println(" km/h");
+
+    Serial.print("Number of satellites = ");
+    Serial.println(satellites);
+
+    Serial.println();  // Print empty line
+
+    // Write sensor data to data file
+    File dataFile = SD.open(filename, FILE_WRITE);
+
+    if (dataFile) {
+      // TODO: See what happens when the sensor is disconnected and we try to write the data
+      String dataString = String(epochTime) + ",";
+      dataString += String(temperature) + ",";
+      dataString += String(humidity) + ",";
+      dataString += String(pressure) + ",";
+      dataString += String(illuminance) + ",";
+      dataString += String(latitude, 7) + ",";
+      dataString += String(longitude, 7) + ",";
+      dataString += String(altitude) + ",";
+      dataString += String(speed) + ",";
+      dataString += String(satellites);
+
+      dataFile.println(dataString);
+      dataFile.close();
+    } else {
+      Serial.println("Error writing to data file!");
+      setLED(HIGH, LOW, LOW);  // RED
+    }
+
+    delay(1000);
   }
-
-  float temperature = ENV.readTemperature();  // MKR ENV Shield
-  float humidity = ENV.readHumidity();
-  float pressure = ENV.readPressure();
-  float illuminance = ENV.readIlluminance();
-  float latitude = GPS.latitude();  // MKR GPS Shield
-  float longitude = GPS.longitude();
-  float altitude = GPS.altitude();
-  float speed = GPS.speed();
-  int satellites = GPS.satellites();
-  unsigned long epochTime = GPS.getTime();
-
-  // Print sensor data to serial
-  Serial.print("Epoch time: ");
-  Serial.println(epochTime);
-
-  Serial.print("Temperature = ");  // MKR ENV Shield
-  Serial.print(temperature);
-  Serial.println(" °C");
-
-  Serial.print("Humidity = ");
-  Serial.print(humidity);
-  Serial.println(" %");
-
-  Serial.print("Pressure = ");
-  Serial.print(pressure);
-  Serial.println(" kPa");
-
-  Serial.print("Illuminance = ");
-  Serial.print(illuminance);
-  Serial.println(" lx");
-
-  Serial.print("Location = ");  // MKR GPS Shield
-  Serial.print(latitude, 7);
-  Serial.print(", ");
-  Serial.println(longitude, 7);
-
-  Serial.print("Altitude = ");
-  Serial.print(altitude);
-  Serial.println("m");
-
-  Serial.print("Ground speed = ");
-  Serial.print(speed);
-  Serial.println(" km/h");
-
-  Serial.print("Number of satellites = ");
-  Serial.println(satellites);
-
-  Serial.println();  // Print empty line
-
-  // Write sensor data to data file
-  File dataFile = SD.open(filename, FILE_WRITE);
-
-  if (dataFile) {
-    // TODO: See what happens when the sensor is disconnected and we try to write the data
-    String dataString = String(epochTime) + ",";
-    dataString += String(temperature) + ",";
-    dataString += String(humidity) + ",";
-    dataString += String(pressure) + ",";
-    dataString += String(illuminance) + ",";
-    dataString += String(latitude, 7) + ",";
-    dataString += String(longitude, 7) + ",";
-    dataString += String(altitude) + ",";
-    dataString += String(speed) + ",";
-    dataString += String(satellites);
-
-    dataFile.println(dataString);
-    dataFile.close();
-  } else {
-    Serial.println("Error writing to data file!");
-    setLED(HIGH, LOW, LOW);  // RED
-  }
-
-  delay(1000);
 }
 
 void setLED(PinStatus red, PinStatus green, PinStatus blue) {
